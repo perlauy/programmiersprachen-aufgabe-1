258274a5fd624c682bd3a44a6ad9ba219ac28d9e
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-04-07 12:12:37 +0200

    Styleguide. floor method changed to floor_aufgabe because VS throws error (re-defined)

--- source/aufgabe.cpp
+++ source/aufgabe.cpp
@@ -3,23 +3,23 @@
 #include <iostream>
 
 int main() {
-	float test1var = 12.564;
-	float fractResult = fract(test1var);
-	std::cout << "Result of fract() of " << test1var << " is " << fractResult << std::endl;
+	float test_var_1 = 12.564;
+	float fract_result = fract(test_var_1);
+	std::cout << "Result of fract() of " << test_var_1 << " is " << fract_result << std::endl;
 
-	int floorResult = floor(test1var);
-	std::cout << "Result of floor() of " << test1var << " is " << floorResult << std::endl;
+	int floor_result = floor_aufgabe(test_var_1);
+	std::cout << "Result of floor() of " << test_var_1 << " is " << floor_result << std::endl;
 
-	unsigned int test2var = 5;
-	bool isPrimeResult = is_prime(test2var);
-	std::cout << "Result of is_prime() of " << test2var << " is " << isPrimeResult << std::endl;		
+	unsigned int test_var_2 = 5;
+	bool is_prime_result = is_prime(test_var_2);
+	std::cout << "Result of is_prime() of " << test_var_2 << " is " << is_prime_result << std::endl;		
 
-	unsigned long factorialResult = factorial(test2var);
-	std::cout << "Result of factorial() of " << test2var << " is " << factorialResult << std::endl;
+	unsigned long factorial_result = factorial(test_var_2);
+	std::cout << "Result of factorial() of " << test_var_2 << " is " << factorial_result << std::endl;
 	
-	unsigned int test3var = 10;
-	unsigned long binomialResult = binomial(test2var, test3var);
-	std::cout << "Result of binomialResult() of " << test2var << " and " << test3var << " is " << binomialResult << std::endl;
+	unsigned int test_var_3 = 10;
+	unsigned long binomial_result = binomial(test_var_3, test_var_2);
+	std::cout << "Result of binomial() of " << test_var_2 << " and " << test_var_3 << " is " << binomial_result << std::endl;
 
 	print_100_primes();
 
--- source/math.hpp
+++ source/math.hpp
@@ -1,7 +1,7 @@
 #ifndef MATH_HPP
 #define MATH_HPP
 float fract(float num);
-int floor(float num);
+int floor_aufgabe(float num);
 bool is_prime(unsigned int num);
 unsigned long factorial(unsigned int num);
 unsigned long binomial(unsigned int a, unsigned int b);
