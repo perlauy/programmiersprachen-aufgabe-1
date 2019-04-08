bec3b6faaf70a4817bc65e674c372ab375619cab
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-04-08 16:44:15 +0200

    Aufgabe 1.8

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,34 @@
-#define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include "math.hpp"
 
-int main(int argc, char* argv[])
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include <cmath>
+
+int gcd (int a, int b)
 {
-	return Catch::Session().run(argc, argv);
+  int result = 1; // Initial result. If a and b have no common divisor, then the result should be 1
+  int rest_a = a; // We divide this number into the factors to test each, and see if they are also divisors of b
+  int rest_b = b;
+
+  while (rest_a > 1) {
+    int prime_factor = get_smallest_prime_divisor(rest_a);
+    if (rest_b % prime_factor == 0) { // b is also divisible by prime_factor
+      result *= prime_factor;
+      rest_b /= prime_factor;
+    }
+    rest_a /= prime_factor;
+  }
+  return result;
+}
+
+TEST_CASE (" describe_gcd ", "[gcd]")
+{
+  REQUIRE (gcd(2, 4) == 2);
+  REQUIRE (gcd(9, 6) == 3);
+  REQUIRE (gcd(3, 7) == 1);
+}
+
+int main (int argc , char * argv [])
+{
+  return Catch::Session().run(argc, argv);
 }
