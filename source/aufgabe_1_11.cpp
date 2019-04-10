2c266e7303e78ca470d4bc770be9bafacc83c63e
Irene Lopez Garcia <irene.lopez.garcia@uni-weimar.de>
2019-04-10 17:21:37 +0200

    Ausgabe 1.11 - Fixed failed test with negative numbers

--- source/aufgabe_1_11.cpp
+++ source/aufgabe_1_11.cpp
@@ -2,12 +2,13 @@
 #include "catch.hpp"
 
 float fract(float num) {
-  return num - floor(num);
+  return num - floor(num) - (num < 0);
 }
 
 TEST_CASE (" fract ", "[fract]")
 {
   REQUIRE(fract(15.12f) == Approx(0.12f));
+  REQUIRE(fract(-15.12f) == Approx(-0.12f));
   REQUIRE(fract(34.9745612385487f) == Approx(0.9745612385487f));
   REQUIRE(fract(0.0f) == Approx(0.0f));
 }
